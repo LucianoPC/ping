e9aef60e1ee6bfc175cb832c30ed5deb93abaad9
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-09-20 20:26:25 -0300

    Fix bug when argc < 3

--- ping.cpp
+++ ping.cpp
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
   if(argc < 3)
   {
     std::cout << "Usage: " << argv[0] << " <address> <port>" << std::endl;
+    return -1;
   }
 
   std::string address = argv[1];
