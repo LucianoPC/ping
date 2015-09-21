c8d0ccb0840594d517e86f852f2a7c989482cad8
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-09-20 21:18:19 -0300

    Fix bug on receive ping message on c++

--- ping.cpp
+++ ping.cpp
@@ -13,14 +13,25 @@ float getRTT(std::string address, int port);
 
 int main(int argc, char *argv[])
 {
-  if(argc < 3)
+  int port = 0;
+  std::string address = "";
+
+  if(argc == 2)
+  {
+    port = 2081;
+  }
+  else if(argc < 3)
   {
     std::cout << "Usage: " << argv[0] << " <address> <port>" << std::endl;
     return -1;
   }
 
-  std::string address = argv[1];
-  int port = atoi(argv[2]);
+  address = argv[1];
+
+  if(port == 0)
+  {
+    port = atoi(argv[2]);
+  }
 
   ping(address, port);
 
@@ -60,9 +71,10 @@ void ping(std::string address, int port)
 
 float getRTT(std::string address, int port)
 {
+  float rtt = -1.0f;
+  int receiveLenght;
   int socketId = socket(AF_INET, SOCK_DGRAM, 0);
   struct sockaddr_in sockaddress;
-  float rtt;
 
   if(socketId < 0) {
     throw std::runtime_error("Could not create socket.");
@@ -73,13 +85,24 @@ float getRTT(std::string address, int port)
   sockaddress.sin_port = htons(port);
   sockaddress.sin_addr.s_addr = inet_addr(address.c_str());
 
+  struct timeval tv;
+  tv.tv_sec = 1;
+  tv.tv_usec = 100;
+
+  char message[4096];
+  socklen_t socklenght = sizeof(sockaddress);
+
   auto initialTime = std::chrono::high_resolution_clock::now();
   sendto(socketId, "", 0, 0, (struct sockaddr*) &sockaddress, sizeof(sockaddress));
+  setsockopt(socketId, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv,sizeof(tv));
+  receiveLenght = recvfrom(socketId, message, 4096, 0, (struct sockaddr *)&sockaddress, &socklenght);
 
-  auto finalTime = std::chrono::high_resolution_clock::now();
-  auto totalTime = std::chrono::duration_cast<std::chrono::microseconds>(finalTime - initialTime).count();
-
-  rtt = ((float)totalTime) / 1000.0f;
+  if(receiveLenght >= 0)
+  {
+    auto finalTime = std::chrono::high_resolution_clock::now();
+    auto totalTime = std::chrono::duration_cast<std::chrono::microseconds>(finalTime - initialTime).count();
+    rtt = ((float)totalTime) / 1000.0f;
+  }
 
   return rtt;
 }
